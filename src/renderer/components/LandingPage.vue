58e655183bec22bd72c662c2bdb46dc5e2976a09
zhangchuanlong <zhangchuanlong@zettakit.com>
2017-09-15 13:28:46 +0800

    0.1.1

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -11,7 +11,7 @@
 
       <div class="right-side">
         <div class="doc">
-          <div class="title">0.1.0</div>
+          <div class="title">0.1.1</div>
           <h2>
             {{message}}
           </h2>
